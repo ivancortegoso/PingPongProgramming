cbb8f1eee5725188a305d4a897b9461aa50c3f30
ivancortegoso <ivancortegoso@cortegoso.com>
2023-07-05 18:27:41 +0200

    cors error corrected

--- PingPongProgramming/Bank-Backend/src/main/java/com/solera/bankbackend/config/security/SecurityConfig.java
+++ PingPongProgramming/Bank-Backend/src/main/java/com/solera/bankbackend/config/security/SecurityConfig.java
@@ -18,6 +18,13 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @AllArgsConstructor
@@ -48,7 +55,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf(csrf -> csrf.disable())
+        http.csrf(csrf -> csrf.disable()).cors(withDefaults())
                 .authorizeHttpRequests((authorize) -> {
                     authorize.requestMatchers("/api/public/**").permitAll();
                     authorize.anyRequest().authenticated();
@@ -68,5 +75,14 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder);
 
         return authProvider;
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("localhost:3000"));
+                configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
