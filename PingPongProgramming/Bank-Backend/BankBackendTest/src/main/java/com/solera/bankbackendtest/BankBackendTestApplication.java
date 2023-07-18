2e9dedd1de638c34935546c35f2585474510b8b6
ivancortegoso <ivan.cortegosobasconcelos@solera.com>
2023-07-18 11:45:22 +0200

    Multimodular project created.

---
+++ PingPongProgramming/Bank-Backend/BankBackendTest/src/main/java/com/solera/bankbackendtest/BankBackendTestApplication.java
@@ -0,0 +1,13 @@
+package com.solera.bankbackendtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackendTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackendTestApplication.class, args);
+	}
+
+}
