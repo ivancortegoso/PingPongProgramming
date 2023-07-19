dfb196ad7836e8b03fc80f18c1eeadca42519590
ivancortegoso <ivancortegoso@cortegoso.com>
2023-07-19 23:40:42 +0200

    API test with rest assured is working now. LoginIT class has one method working, login with correct username and password.

--- PingPongProgramming/Bankbackend/BankBackendIT/src/test/java/LoginIT.java
+++ PingPongProgramming/Bankbackend/BankBackendIT/src/test/java/LoginIT.java
@@ -1,8 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
 public class LoginIT {
     @Test
-    public void loginCorrectUsernameCorrectPassword() {
-
+    public void loginCorrectUsernameCorrectPassword() throws JSONException {
+        RestAssured.baseURI = "http://localhost:8080/";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("username", "ivanucci");
+        requestParams.put("password", "password");
+        RequestSpecification request = RestAssured.given();
+        request.header("Content-Type", "application/json");
+        request.body(requestParams.toString());
+        Response response = request.post("api/public/login");
+        ValidatableResponse validatableResponse = response.then();
+        validatableResponse.statusCode(200);
     }
 }
