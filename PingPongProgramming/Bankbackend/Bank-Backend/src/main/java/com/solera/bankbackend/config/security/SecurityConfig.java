de4827cb04e98fbe24c7d2c5498bd6aa8f943c75
ivancortegoso <ivancortegoso@cortegoso.com>
2023-08-24 21:44:32 +0200

    JWT implemented in Swagger

--- PingPongProgramming/Bankbackend/Bank-Backend/src/main/java/com/solera/bankbackend/config/security/SecurityConfig.java
+++ PingPongProgramming/Bankbackend/Bank-Backend/src/main/java/com/solera/bankbackend/config/security/SecurityConfig.java
@@ -2,6 +2,13 @@ package com.solera.bankbackend.config.security;
 
 import com.solera.bankbackend.config.security.filters.JwtAuthenticationFilter;
 import com.solera.bankbackend.service.UserService;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -79,6 +86,25 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/swagger-bankAPI.html"
     };
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info().title("Bank API")
+                        .description("Bank functions.")
+                        .version("1.0").contact(new Contact().name("Ivan Cortegoso")
+                                .email( "ivancortegoso@hotmail.com"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
